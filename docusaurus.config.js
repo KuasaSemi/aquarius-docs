b02c4fb3137f4b43970ebdf85bd89333223c7e60
mawbydp <91740023+mawbydp@users.noreply.github.com>
2025-05-30 15:24:12 +0100

    Fixing broken links caused by new url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ module.exports = {
         srcDark: "img/logo/kuasasemi-logo-dark.png", // Dark Mode Logo
       },
       items: [
-        { to: "/docs/", label: "Docs", position: "right" },
+        { to: "/", label: "Docs", position: "right" },
         { href: "https://github.com/KuasaSemi", label: "GitHub", position: "right" },
       ],
     },
